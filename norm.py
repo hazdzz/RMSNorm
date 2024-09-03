cdb9df59da7f729980ee4b0219e3645c0a8fbf02
Chieh Chang <raphaelpeo@gmail.com>
2024-09-03 20:49:39 +0800

    improve code

--- norm.py
+++ norm.py
@@ -1,23 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
-from torch import Tensor
+from typing import Union, List, Optional, Tuple
+from torch import Size, Tensor
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dim, eps: float = 1e-5, bias: bool = False) -> None:
+    def __init__(self, normalized_shape: Union[int, List[int], Size], eps: float = 1e-5, bias: bool = False) -> None:
         super(RMSNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        self.normalized_shape = tuple(normalized_shape)
         self.eps = eps
-        self.scale = nn.Parameter(torch.empty(dim))
+        self.weight = nn.Parameter(torch.empty(self.normalized_shape))
         if bias:
-            self.bias = nn.Parameter(torch.empty(dim))
+            self.bias = nn.Parameter(torch.empty(self.normalized_shape))
         else:
             self.register_parameter('bias', None)
 
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        init.ones_(self.scale)
+        init.ones_(self.weight)
         if self.bias is not None:
             init.zeros_(self.bias)
 
@@ -25,8 +29,8 @@ class RMSNorm(nn.Module):
         var = input.pow(2).mean(dim=-1, keepdim=True) + self.eps
         input_norm = input * torch.rsqrt(var)
 
-        rmsnorm = self.scale * input_norm
-
+        rmsnorm = self.weight * input_norm
+        
         if self.bias is not None:
             rmsnorm = rmsnorm + self.bias
 
